9dce6c3e6a5ef7049620c9f4a2c02cd347d29d07
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-16 22:02:39 +0530

    PreProcessor Directives

--- >basics/pre.cpp
+++ >basics/pre.cpp
@@ -1,11 +1,30 @@
-#include <iostream>
-using namespace std;
-#define DEBUG
+// Preprocessor directive to include the standard input-output stream library
+#include <iostream>   
 
-int main(){
-    #ifdef DEBUG
-    cout << "Debug Mode ON";
-#endif
+// Macro definition using the preprocessor
+#define PI 3.14159    
 
-return 0;
+// Use the standard namespace to avoid writing std:: repeatedly
+using namespace std;  
+
+// Main function - execution starts here
+int main() {
+
+    // Declare a variable to store the radius
+    float radius;
+
+    // Ask the user to enter the radius of a circle
+    cout << "Enter the radius of the circle: ";
+    
+    // Take user input and store it in the radius variable
+    cin >> radius;
+
+    // Calculate the area using the macro PI
+    float area = PI * radius * radius;
+
+    // Display the calculated area
+    cout << "The area of the circle is: " << area << endl;
+
+    // Return 0 indicates the program ended successfully
+    return 0;
 }
---
+++ >basics/preprocessor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define DEBUG
+
+int main(){
+    #ifdef DEBUG
+    cout << "Debug Mode ON";
+#endif
+
+return 0;
+}
