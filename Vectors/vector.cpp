88b02b64d08ce3848b96ffdd9daffcbe5d697574
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-22 19:19:30 +0530

    learnt allocations of an vector

--- Vectors/vector.cpp
+++ Vectors/vector.cpp
@@ -7,7 +7,7 @@ int main(){
     vector<int> v = {3,5,8,99,10}; //creating a vector of integers
 
     v.push_back(1);
-                                   //assigning/adding two more elements
+              //assigning/adding two more elements
     v.push_back(9);
 
     v[2] = -9;                      // adding -9 to the 2nd position
