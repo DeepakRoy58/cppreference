e524f1bece749715ba6e710b247c9469b3f4cc26
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-24 20:03:33 +0530

    Alternative solution added to Second Largest

--- Problems (GfG)/2.Second Largest.cpp
+++ Problems (GfG)/2.Second Largest.cpp
@@ -22,3 +22,28 @@ class Solution {
         return second;
     }
 };
+
+
+// ->ALTERNATIVE SOLUTION<-
+
+// class Solution {
+//   public:
+//     int getSecondLargest(vector<int> &arr) {
+        
+//         int n = arr.size();
+
+//         int largest = -1;
+//         int second = -1 ;
+
+//         for (int i = 0; i < n ; i++){
+//             if(arr[i]>largest)
+//             largest = arr[i];
+//         }
+
+//         for (int i = 0;  i < n ; i++ ){
+//             if(arr[i]!=largest)
+//             second = max(arr[i],largest);
+//         }
+//     }
+// };
+
