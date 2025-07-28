2f57ab4dba9b171e103e1ac19a4871a749d7811b
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-28 20:04:01 +0530

    added some decoratives for the second.cpp

--- lecture2/PatternPrinting/second.cpp
+++ lecture2/PatternPrinting/second.cpp
@@ -5,6 +5,7 @@ void print2(int n ){
     for (int i =0;i<n;i++){
         for(int j=0;j<=i;j++){
             cout<< "*-" ;
+            
         }
         cout <<endl;
     }
@@ -13,12 +14,15 @@ void print2(int n ){
 int main() {
 
     int t;
+    cout << "-----------------Enter Your Test Cases-----------------" << endl;
     cin>>t;
     for(int i =0;i<t;i++){
         int n ;
         cin>>n;
         print2(n);
+        
     }
+    cout << "-------------------------------------------------------" << endl;
 
-
+    return 0;
 }
