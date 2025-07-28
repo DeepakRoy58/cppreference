755e4943eaa47915b513ddad6ca70aa68e7b20bd
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-28 20:09:31 +0530

    added decoratives for the pattern1.cpp

--- lecture2/PatternPrinting/one.cpp
+++ lecture2/PatternPrinting/one.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int n;
-    cout<< "Enter Your Number : ";
+    cout<< "----------Enter Your Number : ----------" <<endl;
     cin >> n;
 
     for (int i=0;i<n;i++){
@@ -13,4 +13,6 @@ int main(){
 
         cout<<endl;
     }
+
+    cout << "-----------------------------------------" << endl;
 }
