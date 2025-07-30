f8239678a30013c1a894992a6e668ab50ad87301
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-07-30 14:33:36 +0530

    Covering Basics For Revision

---
+++ LOVE BABBAR/lecture03/if-else.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a,b;
+    cout << "Enter Your First Num: " ;
+    cin >> a;
+
+    cout << "Enter Your Second Num: " ; 
+    cin >> b;
+
+    if(a>b){
+        cout << "THE GIVEN NUMBER IS GREATER THAN SECOND" << endl;
+
+    }
+
+    else {
+        cout << "THE PROVIDED NUMBER IS LESSER THAN THE OTHER" << endl;
+    }
+
+
+}
