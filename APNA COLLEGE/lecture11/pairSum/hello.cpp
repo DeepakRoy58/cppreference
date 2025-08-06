417c0184c68882670397df9ee5a1e2738fee44aa
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-06 20:42:39 +0530

    Power (X N)

---
+++ APNA COLLEGE/lecture11/pairSum/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int a ;
+    int b;
+
+    cout << "ENter your 1st nu: " ;
+    cin >> a;
+
+    cout << "Enter your 2nd num:";
+    cin >> b;
+
+    cout << "Your Total Sum is : " << a+b << " "<< endl;
+
+    return 0;
+}
