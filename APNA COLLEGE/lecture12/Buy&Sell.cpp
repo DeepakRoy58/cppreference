13d0d5595aad6f8322746d7397becdd76f38dab3
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-07 20:54:03 +0530

    Buy & sell Stock

---
+++ APNA COLLEGE/lecture12/Buy&Sell.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int bestBuy = prices[0];
+
+        for(int i = 1 ; i < prices.size() ; i++) {
+            if(prices[i] > bestBuy){
+                maxProfit = max ( maxProfit , prices[i] - bestBuy );
+
+            }
+
+            bestBuy = min (prices[i] , bestBuy);
+        }
+
+        return maxProfit;
+    }
+};
