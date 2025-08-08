d78e086dd17ec180ae03dd6511fdd6b50b8a6ac2
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-08 22:38:33 +0530

    maxx water in a container

---
+++ APNA COLLEGE/lecture13/maxWater.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxWater = 0;
+        int n = height.size();
+
+        for (int leftBound = 0; leftBound < n; leftBound++) {
+            for (int rightBound = leftBound + 1; rightBound < n; rightBound++) {
+                int width = rightBound - leftBound;
+                int ht = min(height[leftBound], height[rightBound]);  // renamed here
+                int area = width * ht;
+                maxWater = max(area, maxWater);
+            }
+        }
+        return maxWater;
+    }
+};
